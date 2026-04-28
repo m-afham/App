5f655b2731e40a60b11850cba0b7ef584ebb1657
Chuck Dries <chuck@expensify.com>
2026-04-28 10:03:17 -0700

    Pass reasonCode at resendValidateCode/requestValidateCodeAction call sites

--- src/pages/OnboardingPrivateDomain/BaseOnboardingPrivateDomain.tsx
+++ src/pages/OnboardingPrivateDomain/BaseOnboardingPrivateDomain.tsx
@@ -49,7 +49,7 @@ function BaseOnboardingPrivateDomain({shouldUseNativeStyles, route}: BaseOnboard
         if (!email) {
             return;
         }
-        resendValidateCode(email);
+        resendValidateCode(email, {reasonCode: CONST.VALIDATE_CODE_REASON.VALIDATE_ACCOUNT});
     }, [email]);
 
     const handleBackButtonPress = useCallback(() => {
